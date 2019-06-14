b4e50e3a0c718684fea7584138fceb94abd752c3
liwangzhi <liwangzhi@126.com>
2019-06-14 16:59:14 +0800

    data form

---
+++ demo/dataform_msgpack/dataform_msgpack.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+    "fmt"
+    "github.com/vmihailenco/msgpack"
+    "io/ioutil"
+    "math/rand"
+)
+
+type Person struct {
+    Name string
+    Age int
+    Sex string
+}
+
+var (
+    filename = "d:/logs/person.dat"
+)
+
+func main() {
+    err := testWriteJson(filename)
+    if err != nil {
+        fmt.Printf("write json failed, err:%v\n", err)
+        return
+    }
+
+    err = testReadJson(filename)
+    if err != nil {
+        fmt.Printf("read json failed, err:%v\n", err)
+        return
+    }
+}
+
+func testReadJson(filename string) (err error) {
+    var persons []*Person
+    data, err := ioutil.ReadFile(filename)
+    if err != nil {
+        return
+    }
+
+    err = msgpack.Unmarshal(data, &persons)
+    if err != nil {
+        return
+    }
+
+    for _, v := range persons {
+        fmt.Printf("%#v\n", v)
+    }
+    return
+}
+
+func testWriteJson(filename string) (err error) {
+    var persons []*Person
+    for i := 0; i < 10; i++ {
+        p := &Person{
+            Name: fmt.Sprintf("name%d", i),
+            Age: rand.Intn(100),
+            Sex: "Man",
+        }
+
+        persons = append(persons, p)
+    }
+    data, err := msgpack.Marshal(persons)
+    if err != nil {
+        fmt.Printf("=marshal failed, err:%v\n", err)
+        return
+    }
+
+    err = ioutil.WriteFile(filename, data, 0755)
+    if err != nil {
+        fmt.Printf("write file failed, err:%v\n", err)
+        return
+    }
+    return
+}
