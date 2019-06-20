536ec07769840bea764dc007371c1d99187da140
liwangzhi <liwangzhi@126.com>
2019-06-20 21:16:02 +0800

    delete garyburd redigo

--- demo/redis/redis.go
+++ demo/redis/redis.go
@@ -2,18 +2,19 @@ package main
 
 import (
     "fmt"
-    "github.com/garyburd/redigo/redis"
+    "github.com/gomodule/redigo/redis"
 )
 
 func main() {
-    c, err := redis.Dial("tcp", "192.168.99.221:6379")
+    option := redis.DialPassword("foobared")
+    c, err := redis.Dial("tcp", "192.168.99.221:6379", option)
     //c, err := redis.Dial("tcp", "192.168.99.221:7000,192.168.99.221:6380,192.168.99.222:6380,192.168.99.221:6381")
     if err != nil {
         fmt.Println("conn redis failed, ", err)
         return
     }
     defer c.Close()
-    _, err = c.Do("Set", "abc", 100)
+    _, err = c.Do("Set", "abc", 102)
     if err != nil {
         fmt.Println(err)
         return
