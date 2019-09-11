ffc1744925b4616ffa0bd4b8b21840c1424e8a38
liwangzhi <liwangzhi@126.com>
2019-09-11 11:56:18 +0800

    逆波兰式计算案例

---
+++ project/cals_rpn/calc/stack.go
@@ -0,0 +1,38 @@
+package calc
+
+import "fmt"
+
+type Stack struct {
+    data [1024]string
+    top  int
+}
+
+// Push data to stack
+func (s *Stack) Push(d string) {
+    s.data[s.top] = d
+    s.top++
+}
+
+func (s *Stack) Pop() (ret string, err error) {
+    if s.top == 0 {
+        err = fmt.Errorf("stack is empty")
+        return
+    }
+    s.top--
+    ret = s.data[s.top]
+    return
+}
+
+// 查看栈顶的元素，而不出栈
+func (s *Stack) Top() (ret string, err error) {
+    if s.top == 0 {
+        err = fmt.Errorf("stack is empty")
+        return
+    }
+    ret = s.data[s.top - 1]
+    return
+}
+
+func (s *Stack) Empty() bool {
+    return s.top == 0
+}
